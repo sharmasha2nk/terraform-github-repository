474ab5eadcdabaca3430be08ea32f8af46b4710d
Shashank Sharma <shashank.sharma@traveloka.com>
2022-03-15 14:21:28 +0800

    New variables and resources for autolink and Github apps

--- main.tf
+++ main.tf
@@ -4,22 +4,23 @@ data "github_team" "team_ids" {
 }
 
 resource "github_repository" "main" {
-  name               = var.name
-  description        = var.description
-  homepage_url       = var.homepage_url
-  private            = var.private
-  has_issues         = var.has_issues
-  has_wiki           = var.has_wiki
-  allow_merge_commit = var.allow_merge_commit
-  allow_squash_merge = var.allow_squash_merge
-  allow_rebase_merge = var.allow_rebase_merge
-  has_downloads      = var.has_downloads
-  auto_init          = var.auto_init
-  gitignore_template = var.gitignore_template
-  license_template   = var.license_template
-  default_branch     = var.default_branch
-  archived           = var.archived
-  topics             = var.topics
+  name                   = var.name
+  description            = var.description
+  homepage_url           = var.homepage_url
+  private                = var.private
+  has_issues             = var.has_issues
+  has_wiki               = var.has_wiki
+  allow_merge_commit     = var.allow_merge_commit
+  allow_squash_merge     = var.allow_squash_merge
+  allow_rebase_merge     = var.allow_rebase_merge
+  has_downloads          = var.has_downloads
+  auto_init              = var.auto_init
+  gitignore_template     = var.gitignore_template
+  license_template       = var.license_template
+  default_branch         = var.default_branch
+  archived               = var.archived
+  topics                 = var.topics
+  delete_branch_on_merge = var.delete_branch_on_merge
 
   dynamic "template" {
     for_each = (var.template_repository != "") ? ["dummy"] : []
@@ -31,9 +32,10 @@ resource "github_repository" "main" {
 }
 
 resource "github_branch_protection" "main" {
-  pattern        = github_repository.main.default_branch
-  repository_id  = github_repository.main.node_id
-  enforce_admins = var.enforce_admins
+  pattern                         = github_repository.main.default_branch
+  repository_id                   = github_repository.main.node_id
+  enforce_admins                  = var.enforce_admins
+  require_conversation_resolution = var.require_conversation_resolution
 
   required_status_checks {
     strict   = var.force_pr_rebase
@@ -58,4 +60,17 @@ resource "github_repository_collaborator" "this" {
   repository = github_repository.main.name
   username   = each.key
   permission = each.value
+}
+
+resource "github_repository_autolink_reference" "this" {
+  for_each            = var.github_repository_autolink_references
+  repository          = github_repository.main.name
+  key_prefix          = each.key
+  target_url_template = each.value
+}
+
+resource "github_app_installation_repository" "this" {
+  count              = length(var.github_app_installations)
+  installation_id    = var.github_app_installations[count.index]
+  repository         = github_repository.main.name
 }
